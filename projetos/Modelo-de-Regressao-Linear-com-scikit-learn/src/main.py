e31aba4577e72e48cb3673c9c78f966ad625ae9d
Eduardo Rabelo Feitosa <eduardo.rblof@gmail.com>
2026-04-09 20:12:11 -0300

    reestrutura repositório e adiciona projetos

--- projetos/Modelo-de-Regressao-Linear-com-scikit-learn/src/main.py
+++
@@ -1,5 +0,0 @@
-from gerar_dados import gerar_dados
-from modelo_reg_linear import analisar
-
-gerar_dados()
-analisar()
--- projetos/Modelo-de-Series-com-Python/src/main.py
+++
@@ -1,5 +0,0 @@
-from gerar_dados import gerar_dados
-from analise_series import analisar
-
-gerar_dados()
-analisar()
